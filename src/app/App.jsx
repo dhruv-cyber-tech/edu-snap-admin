4ab41e10ab4956741ad09c894a6e5d7a6b289baf
Dhruv Ghosiya <dhruv251106@gmail.com>
2026-06-23 21:54:02 +0530

    Add sonner dependency

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Users, Wallet, CalendarCheck, ClipboardList } from "lucide-react";
 
-import { Toaster } from "@/components/ui/sonner";
-import OfflineBanner from "@/components/OfflineBanner";
-import ProtectedRoute from "@/components/ProtectedRoute";
+import { Toaster } from "../components/ui/sonner";
+import OfflineBanner from "../components/OfflineBanner";
+import ProtectedRoute from "../components/ProtectedRoute";
 import AppLayout from "@/components/AppLayout";
 import Login from "@/pages/Login";
 import Dashboard from "@/pages/Dashboard";
