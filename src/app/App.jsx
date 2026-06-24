5d7c15b3b62db7d9375dd1e031eb18fd3ac46829
Dhruv Ghosiya <dhruv251106@gmail.com>
2026-06-24 09:13:11 +0530

    Fix build issues

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "..tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Users, Wallet, CalendarCheck, ClipboardList } from "lucide-react";
 
 import { Toaster } from "../components/ui/sonner";
