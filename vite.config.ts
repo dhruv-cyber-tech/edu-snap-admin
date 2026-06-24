597e25a7e3c648f5d7daa51df88bed54815fe019
Dhruv Ghosiya <dhruv251106@gmail.com>
2026-06-24 09:06:39 +0530

    Add explicit path alias to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
-import tsconfigPaths from 'vite-tsconfig-paths'; 
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     tanstackStart({ server: { entry: "server" } }), 
     nitro(), 
     viteReact(),
-    tsconfigPaths() 
+    tsconfigPaths()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
