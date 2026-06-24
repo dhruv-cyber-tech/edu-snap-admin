b3930cfa7d150eebfedebf5eba35f0e035e85d44
Dhruv Ghosiya <dhruv251106@gmail.com>
2026-06-24 09:00:35 +0530

    Revert to TanStack Start config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
-import path from 'path';
+import { nitro } from 'nitro/vite';
+import tsconfigPaths from 'vite-tsconfig-paths'; 
 
 export default defineConfig({
   plugins: [
+    tanstackStart({ server: { entry: "server" } }), 
+    nitro(), 
     viteReact(),
+    tsconfigPaths() 
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
